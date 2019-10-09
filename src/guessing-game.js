cf1c1cdf014090973cf4fdcfdce58fead7b8f26e
Masha <you@example.com>
2019-10-10 01:22:17 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min_value = min;
+        this.max_value = max;
     }
 
     guess() {
-
+        this.candidate = Math.round((this.min_value+this.max_value)/2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max_value = this.candidate;
     }
 
     greater() {
-
+        this.min_value = this.candidate;
     }
 }
 
